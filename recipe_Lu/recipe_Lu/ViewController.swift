a0527cf21090c57413d5b052ea1bb46359c13a56
Martin_Z_Lu <luzhilin17@hotmail.com>
2019-12-03 02:02:32 -0800

    first try do a app by myself

---
+++ recipe_Lu/recipe_Lu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  recipe_Lu
+//
+//  Created by Han on 2019-12-03.
+//  Copyright © 2019 Martin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
